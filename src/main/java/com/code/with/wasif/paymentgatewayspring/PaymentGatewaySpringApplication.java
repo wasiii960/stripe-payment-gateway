12ed17a5c87e6268e57f0c2af4e8b66ed53ee489
wasif960 <wasif.ali@sltn.net>
2024-07-23 15:40:04 +0500

    stripe payment gateway

---
+++ src/main/java/com/code/with/wasif/paymentgatewayspring/PaymentGatewaySpringApplication.java
@@ -0,0 +1,13 @@
+package com.code.with.wasif.paymentgatewayspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewaySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGatewaySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/with/wasif/paymentgatewayspring/PaymentGatewaySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.with.wasif.paymentgatewayspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewaySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
